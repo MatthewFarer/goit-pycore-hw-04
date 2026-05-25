517c94a9e859308de7746e5b4579bee7f575249b
Mykyta Ihnatenko <matthewnfarer@gmail.com>
2026-05-25 03:30:38 +0300

    innit

---
+++ task_2_get_cats_info.py
@@ -0,0 +1,31 @@
+def get_cats_info(path):
+    cats = []
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(",")
+
+                cat_info = {
+                    "id": cat_id,
+                    "name": name,
+                    "age": age
+                }
+
+                cats.append(cat_info)
+
+        return cats
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return []
+
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+        return []
+
+
+# Приклад використання
+cats_info = get_cats_info("cats_file.txt")
+
+print(cats_info)
