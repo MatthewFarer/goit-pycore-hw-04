1d88fb630152fef1bf59ec5494611ca6f64a43bd
Mykyta Ihnatenko <matthewnfarer@gmail.com>
2026-05-25 03:07:56 +0300

    innit

---
+++ task_1_total_salary.py
@@ -0,0 +1,30 @@
+def total_salary(path):
+    total = 0
+    count = 0
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                name, salary = line.strip().split(",")
+                total += int(salary)
+                count += 1
+
+        average = total / count if count > 0 else 0
+        average = int(average)
+
+        return total, average
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return 0, 0
+
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+        return 0, 0
+
+
+# path to the file
+total, average = total_salary("salary_file.txt")
+
+print(f"Загальна сума заробітної плати: {total}")
+print(f"Середня заробітна плата: {average}")
